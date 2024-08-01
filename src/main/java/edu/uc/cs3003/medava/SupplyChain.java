30b88795a84106a51432a3e50e8a59f01e36931a
adnval <kevin6700@proton.me>
2024-07-31 23:27:15 -0400

    Initial Commit

---
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -0,0 +1,8 @@
+package edu.uc.cs3003.medava;
+
+public class SupplyChain {
+  public static void main(String args[]) {
+    // System.out.println("Hello, Kevin! This is the WOPR speaking.");
+    HospitalRunner.run();
+  }
+}
